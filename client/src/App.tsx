6303146ead5054aaf8d8455f445781e03d3de918
shuhei.morioka <selmertsx@gmail.com>
2018-09-09 17:29:03 +0900

    routing sample codes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,42 @@
 import * as React from "react";
 import { BrowserRouter, Link, Route } from 'react-router-dom'
 
-export class App extends React.Component {
-  public render() {
-    const tasks = [{ title: "TODO１つ目", id: 0 }];
-    return(
-      <div>
-        <h1> TODO App </h1>
-        <ul>
-          <li>
-            { tasks[0].title }
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to='/friends'>Friends</Link></li>
+      </ul>
+
+      <hr />
+
+      <Route exact={true} path='/' component={Home} />
+      <Route path='/about' component={About} />
+      <Route path='/friends' component={Friends} />
+    </div>
+  </BrowserRouter>
+)
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <p>Welcome to ようこそ</p>
+  </div>
+)
+const About = () => (
+  <div>
+    <h2>About</h2>
+    <p>フレンズに投票するページです</p>
+  </div>
+)
+const Friends = () => (
+  <div>
+    <h2>Friends</h2>
+    <p>ここにフレンズのリストを書きます</p>
+  </div>
+)
+
+export { App }
+
