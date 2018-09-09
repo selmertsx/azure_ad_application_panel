fccbc44328e0878d5121c509bc04dca7676e3d7c
shuhei.morioka <selmertsx@gmail.com>
2018-09-09 16:40:25 +0900

    npm i -D webpack-dev-server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
 class App extends React.Component {
   public render() {
