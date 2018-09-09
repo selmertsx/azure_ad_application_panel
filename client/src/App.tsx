cf66fc317babd7499bafca35be2fcb783f6d0d45
shuhei.morioka <selmertsx@gmail.com>
2018-09-09 18:06:51 +0900

    App classについてComponent classを継承するようにした

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,29 @@
-import * as React from "react";
+import * as React  from "react";
 import { BrowserRouter, Link, Route } from 'react-router-dom'
 import { TopPage } from "./components/TopPage";
 import { Tools } from "./components/Tools";
 
-const App = () => (
-  <BrowserRouter>
-    <div>
-      <ul>
-        <li><Link to="/">TopPage</Link></li>
-        <li><Link to="/tools">Tools</Link></li>
-      </ul>
+interface AppState { }
+interface AppProps { }
 
-      <hr />
-      <Route exact={true} path='/' component={TopPage} />
-      <Route path='/tools' component={Tools} />
-    </div>
-  </BrowserRouter>
-)
+class App extends React.Component <AppProps, AppState> {
+  public render() {
+    return(
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li><Link to="/">TopPage</Link></li>
+            <li><Link to="/tools">Tools</Link></li>
+          </ul>
+
+          <hr />
+          <Route exact={true} path='/' component={TopPage} />
+          <Route path='/tools' component={Tools} />
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
 
 export { App }
 
--- client/src/components/TopPage.tsx
+++ client/src/components/TopPage.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 
+
+
 export const TopPage = () => (
   <div>
     <h2>Home</h2>
