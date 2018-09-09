198d7ea378abf9a79d346a11b4fa2c812f2f332e
shuhei.morioka <selmertsx@gmail.com>
2018-09-09 17:34:12 +0900

    create TopPage Component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,40 +1,27 @@
 import * as React from "react";
 import { BrowserRouter, Link, Route } from 'react-router-dom'
+import { TopPage } from "./components/TopPage";
 
 const App = () => (
   <BrowserRouter>
     <div>
       <ul>
-        <li><Link to="/">Home</Link></li>
+        <li><Link to="/">TopPage</Link></li>
         <li><Link to="/about">About</Link></li>
-        <li><Link to='/friends'>Friends</Link></li>
       </ul>
 
       <hr />
 
-      <Route exact={true} path='/' component={Home} />
+      <Route exact={true} path='/' component={TopPage} />
       <Route path='/about' component={About} />
-      <Route path='/friends' component={Friends} />
     </div>
   </BrowserRouter>
 )
 
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-    <p>Welcome to ようこそ</p>
-  </div>
-)
 const About = () => (
   <div>
     <h2>About</h2>
     <p>フレンズに投票するページです</p>
-  </div>
-)
-const Friends = () => (
-  <div>
-    <h2>Friends</h2>
-    <p>ここにフレンズのリストを書きます</p>
   </div>
 )
 
---
+++ client/src/components/TopPage.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+export const TopPage = () => (
+  <div>
+    <h2>Home</h2>
+    <p>Welcome to ようこそ</p>
+  </div>
+)
