57f1c482b77818e43299c8d219dc9a982814438c
shuhei.morioka <selmertsx@gmail.com>
2018-09-09 16:43:26 +0900

    [webpack-dev-server] set webpack-dev-server settings

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -12,5 +12,13 @@ module.exports = {
     rules: [
       { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
     ]
-  }
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '/src/static'), // index.htmlの格納場所
+    historyApiFallback: true, // history APIが404エラーを返す場合にindex.htmlに飛ばす
+    inline: true, // ソース変更時リロードモード
+    hot: true, // HMR(Hot Module Reload)モード
+    port: config.get('PORT') + 1, // 起動ポート,
+    host: '0.0.0.0',
+  },
 }
