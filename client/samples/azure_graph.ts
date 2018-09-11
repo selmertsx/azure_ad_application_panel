2591c35c8b28480abab9bb7c79d789ea0cc78433
shuhei.morioka <selmertsx@gmail.com>
2018-09-11 23:00:04 +0900

    npm install @types/microsoft/microsoft-graph-client

--- client/samples/azure_graph.ts
+++ client/samples/azure_graph.ts
@@ -1,4 +1,12 @@
+import { Client } from "@microsoft/microsoft-graph-client";
+
 const APP_ID = process.env.APP_ID;
 const APP_KEY = process.env.APP_KEY;
 const GRAPH_API = process.env.GRAPH_API;
 const OAUTH_TOKEN_ENDPOINT = process.env.OAUTH_TOKEN_ENDPOINT;
+
+const client: Client = Client.init({
+  authProvider: done => {
+    done(null, "PassInAccessTokenHere");
+  }
+});
