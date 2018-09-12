be45b4991ed1ae7f9546e7397b76ff51d7373e5b
shuhei.morioka <selmertsx@gmail.com>
2018-09-12 21:08:31 +0900

    create ToolContainer

---
+++ client/src/containers/ToolContainer.ts
@@ -0,0 +1,25 @@
+import { connect } from "react-redux";
+import { Dispatch } from "redux";
+
+import { addTool, inputTool } from "../actions/tools";
+import { Tools } from "../components/Tools";
+import { ToolAction, ToolState } from "../Types";
+
+function mapStateToProps({ tool, tools }: ToolState): ToolState {
+  return {
+    tool,
+    tools
+  };
+}
+
+function mapDispatchToProps(dispatch: Dispatch<ToolAction>) {
+  return {
+    addTool: (tool: string) => dispatch(addTool(tool)),
+    inputTool: (tool: string) => dispatch(inputTool(tool))
+  };
+}
+
+export const AppContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Tools);
